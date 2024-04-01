2bfa286ef5cf9a2483ddba9aabea9d1218fe0584
Lolett Llanquinao <lmarialolett@yahoo.es>
2024-03-31 21:38:14 -0300

    desafio 4

---
+++ dia13_desafio4/filtro.py
@@ -0,0 +1,56 @@
+"""
+La empresa tiene un contrato con una tienda de tecnología en la cual quieren implementar un
+filtrado por precio. Para ello se solicita generar el archivo filtro.py con la solución al
+problema. Dada una muestra de los productos que actualmente se encuentran disponibles en
+la tienda (un diccionario), se solicita implementar una función que permita entregar lo
+siguiente:
+● Un diccionario con los productos que cumplen una cierta condición dado un umbral
+● La función debe permitir mostrar los valores mayor que o menor que un umbral
+(siempre estrictos).
+● Por defecto la función debe siempre mostrar los valores mayor que el umbral a menos
+que se indique lo contrario.
+"""
+
+import sys
+
+precios = {
+    'Notebook': 700000,
+    'Teclado': 25000,
+    'Mouse': 12000,
+    'Monitor': 250000,
+    'Escritorio': 135000,
+    'Tarjeta de Video': 1500000
+}
+
+def filtrar_por_precio(precios, umbral, condicion='mayor'):
+    
+    #Filtra los productos según un umbral dado y una comparación especificada.
+    
+    if condicion == 'menor':
+        productos_filtrados = {producto: precio for producto, precio in precios.items() if precio < umbral}
+    elif condicion == 'mayor':
+        productos_filtrados = {producto: precio for producto, precio in precios.items() if precio > umbral}
+    else:
+        return "Lo sentimos, no es una operación válida"
+    
+    return productos_filtrados
+
+if len(sys.argv) == 2:
+        umbral = int(sys.argv[1])
+        resultado = filtrar_por_precio(precios, umbral)
+        print("Los productos mayores al umbral son:", ', '.join(resultado.keys()))
+elif len(sys.argv) == 3:
+        umbral = int(sys.argv[1])
+        condicion = sys.argv[2].lower()
+        resultado = filtrar_por_precio(precios, umbral, condicion)
+        if isinstance(resultado, dict):
+            print("Los productos", condicion, "al umbral son:", ', '.join(resultado.keys()))
+        else:
+            print(resultado)
+else:
+        print("Uso: python filtro.py <umbral> [mayor|menor]")
+        
+        #python3 dia13_desafio4/filtro.py 30000  Los productos mayores al umbral son: Notebook, Monitor, Escritorio, Tarjeta de Video
+        #python3 dia13_desafio4/filtro.py 30000 menor Los productos menor al umbral son: Teclado, Mouse
+        #python3 dia13_desafio4/filtro.py 30000 otro Lo sentimos, no es una operación válida
+        
---
+++ dia13_desafio4/ong.py
@@ -0,0 +1,58 @@
+"""    
+El factorial se define de la siguiente forma:
+𝑛! = 𝑛 ∗ 𝑛 − 1 ∗ 𝑛 − 2 ∗ . . .∗ 2 ∗ 1
+En un ejemplo práctico, el factorial de 5 (5!) se calculará como:
+5! = 5 ∗ 4 ∗ 3 ∗ 2 ∗ 1 = 120
+Por otro lado la productoria se define como la multiplicación de elementos.
+𝐴 = [3,6,4,2,8]
+∏ 𝐴𝑖 = 3 ∗ 6 ∗ 4 ∗ 2 ∗ 8
+Para resolver este programa se solicita lo siguiente:
+● Crear un script llamado ong.py que contenga las siguientes funciones:
+○ Una función que calcule el factorial.
+○ Una función que calcule la productoria.
+○ Una función que permita controlar los cálculos. Esta función se debe invocar
+de la siguiente manera:
+calcular(fact_1 = 5, prod_1 = [3,6,4,2,8], fact_2 = 6)
+_ 5
+www.desafiolatam.com
+Se ingresarán un valor numérico como argumento con el nombre fact_i cuando se requiera
+calcular un factorial, y una lista como argumento prod_i cuando se requiera calcular una
+productoria. Cabe destacar que la función debe permitir ingresar estos argumentos en
+cualquier orden y en cualquier cantidad. El resultado de la función se debe imprimir en pantalla
+de la siguiente forma:
+El factorial de 5 es 120
+La productoria de [4, 6, 7, 4, 3] es 2016
+El factorial de 6 es 720
+"""
+
+def factorial(numero): #Calcula el factorial de un número entero positivo
+    
+    if numero == 0:
+        return 1
+    else:
+        return numero * factorial(numero - 1)
+
+def productoria(lista): #Calcula la productoria de los elementos de una lista.
+    valor = 1
+    for num in lista:
+        valor *= num
+    return valor
+
+def calcular(**kwargs): #Controla los cálculos de factorial y productoria según los argumentos proporcionados.
+    
+    for clave, valor in kwargs.items():
+     if clave in "factorial":
+            n = int(valor)
+            resultado = factorial(n)
+            print(f"El factorial de {valor} es {resultado}")
+     elif clave in "productoria":
+            lista = valor
+            resultado = productoria(lista)
+            print(f"La productoria de {lista} es {resultado}")
+
+calcular(factorial=5)  #El factorial de 5 es 120
+calcular(productoria=[3, 6, 4, 2, 8]) #La productoria de [3, 6, 4, 2, 8] es 1152
+calcular(factorial=6) #El factorial de 6 es 720
+
+#Profe, me ayude mucho con la IA. creo que necesito reforzar mucho mas esta materia porque no me senti preparada como para completar sola esta mision.
+# y eso que cuando usted lo explica en clases lo comprendo, luego cuando debo realizar sola el ejercicio me cuesta empezar y resolver. 
